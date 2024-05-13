78e19ea5332d24a1c053c5f441b1d980ccd4fd8e
unknown <fabio.hkcesare@outlook.com>
2024-05-13 17:05:13 -0300

    Segunda commit

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template
 from flask import request
 
+# As rotas são para poder definir os caminhos
 @app.route('/')#Configurando uma rota web '/' == vazio, ao entrar no site vazio(sem a /) vai para o index.html
 @app.route('/index')#Tanto '/' quanto '/index', levam para index.html
 def index():
@@ -10,3 +11,7 @@ def index():
 @app.route('/contato')
 def contato():
     return render_template('contato.html', titulo="Contato", nome="Fabio")
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html', titulo="Cadastro", nome="Fabio")
